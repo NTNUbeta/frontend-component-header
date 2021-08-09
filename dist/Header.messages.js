38caabe7086274093e20dfe01f53f2e18563d5c0
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-08-09 17:24:52 +0200

    Fixing messages in header

--- dist/Header.messages.js
+++ dist/Header.messages.js
@@ -1,103 +1,190 @@
-//import { defineMessages } from "@edx/frontend-platform/i18n";
+var _defineMessages;
 
-/*
-const messages = defineMessages({
-  'header.links.courses': {
-    id: 'header.links.courses',
-    defaultMessage: 'Courses',
-    description: 'Link to the learner course dashboard',
-  },
-  'header.links.programs': {
-    id: 'header.links.programs',
-    defaultMessage: 'Programs',
-    description: 'Link to the learner program dashboard',
-  },
-  'header.links.content.search': {
-    id: 'header.links.content.search',
-    defaultMessage: 'Discover New',
-    description: 'Link to the content search page',
-  },
-  'header.links.schools': {
-    id: 'header.links.schools',
-    defaultMessage: 'Schools & Partners',
-    description: 'Link to the schools and partners landing page',
-  },
-  'header.user.menu.dashboard': {
-    id: 'header.user.menu.dashboard',
-    defaultMessage: 'Dashboard',
-    description: 'Link to the user dashboard',
-  },
-  'header.user.menu.profile': {
-    id: 'header.user.menu.profile',
-    defaultMessage: 'Profile',
-    description: 'Link to the user profile',
-  },
-  'header.user.menu.account.settings': {
-    id: 'header.user.menu.account.settings',
-    defaultMessage: 'Account',
-    description: 'Link to account settings',
-  },
-  'header.user.menu.order.history': {
-    id: 'header.user.menu.order.history',
-    defaultMessage: 'Order History',
-    description: 'Link to order history',
-  },
-  'header.user.menu.logout': {
-    id: 'header.user.menu.logout',
-    defaultMessage: 'Logout',
-    description: 'Logout link',
-  },
-  'header.user.menu.login': {
-    id: 'header.user.menu.login',
-    defaultMessage: 'Login',
-    description: 'Login link',
-  },
-  'header.user.menu.register': {
-    id: 'header.user.menu.register',
-    defaultMessage: 'Sign Up',
-    description: 'Link to registration',
-  },
-  'header.label.account.nav': {
-    id: 'header.label.account.nav',
-    defaultMessage: 'Account',
-    description: 'The aria label for the account menu nav',
-  },
-  'header.label.account.menu': {
-    id: 'header.label.account.menu',
-    defaultMessage: 'Account Menu',
-    description: 'The aria label for the account menu trigger',
-  },
-  'header.label.account.menu.for': {
-    id: 'header.label.account.menu.for',
-    defaultMessage: 'Account menu for {username}',
-    description: 'The aria label for the account menu trigger when the username is displayed in it',
-  },
-  'header.label.main.nav': {
-    id: 'header.label.main.nav',
-    defaultMessage: 'Main',
-    description: 'The aria label for the main menu nav',
-  },
-  'header.label.main.menu': {
-    id: 'header.label.main.menu',
-    defaultMessage: 'Main Menu',
-    description: 'The aria label for the main menu trigger',
-  },
-  'header.label.main.header': {
-    id: 'header.label.main.header',
-    defaultMessage: 'Main',
-    description: 'The aria label for the main header',
-  },
-  'header.label.secondary.nav': {
-    id: 'header.label.secondary.nav',
-    defaultMessage: 'Secondary',
-    description: 'The aria label for the seconary nav',
-  },
-  'header.label.skip.nav': {
-    id: 'header.label.skip.nav',
-    defaultMessage: 'Skip to main content',
-    description: 'A link used by screen readers to allow users to skip to the main content of the page.',
-  },
-});
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-export default messages; */
+/* eslint-disable quotes */
+
+/* eslint-disable no-dupe-keys */
+import { defineMessages } from "@edx/frontend-platform/i18n";
+var messages = defineMessages((_defineMessages = {
+  "header.links.home": {
+    id: "header.links.home",
+    defaultMessage: "Home",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.links.courses": {
+    id: "header.links.courses",
+    defaultMessage: "Courses",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.links.articles": {
+    id: "header.links.articles",
+    defaultMessage: "Articles",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.links.programs": {
+    id: "header.links.programs",
+    defaultMessage: "Programs",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.links.search": {
+    id: "header.links.search",
+    defaultMessage: "Search",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.links.about": {
+    id: "header.links.about",
+    defaultMessage: "About",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.user.menu.lms.dashboard": {
+    id: "header.user.menu.lms.dashboard",
+    defaultMessage: "LMS Dashboard",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.user.menu.profile": {
+    id: "header.user.menu.profile",
+    defaultMessage: "Profile",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.user.menu.lms.account.settings": {
+    id: "header.user.menu.lms.account.settings",
+    defaultMessage: "LMS Settings",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.user.menu.logout": {
+    id: "header.user.menu.logout",
+    defaultMessage: "Logout",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.user.menu.login": {
+    id: "header.user.menu.login",
+    defaultMessage: "Login",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.user.menu.register": {
+    id: "header.user.menu.register",
+    defaultMessage: "Register",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.user.menu.dashboard": {
+    id: "header.user.menu.dashboard",
+    defaultMessage: "Dashboard",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.label.account.menu.for": {
+    id: "header.user.menu.for",
+    defaultMessage: "menu.for",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.label.main.nav": {
+    id: "header.user.menu.nav",
+    defaultMessage: "main.nav",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.label.secondary.nav": {
+    id: "header.user.secondary.nav",
+    defaultMessage: "secondary.nav",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.label.main.header": {
+    id: "header.label.main.header",
+    defaultMessage: "label.main.header",
+    description: "A message that proves that internationalization is working."
+  },
+  "header.label.main.menu": {
+    id: "header.label.main.menu",
+    defaultMessage: "header.label.main.menu",
+    description: "header.label.main.menu"
+  }
+}, _defineProperty(_defineMessages, "header.label.main.nav", {
+  id: "header.label.main.nav",
+  defaultMessage: "header.label.main.nav",
+  description: "header.label.main.nav"
+}), _defineProperty(_defineMessages, "header.label.secondary.nav", {
+  id: "header.label.secondary.nav",
+  defaultMessage: "header.label.secondary.nav",
+  description: "header.label.secondary.nav"
+}), _defineProperty(_defineMessages, "header.label.account.menu", {
+  id: "header.label.account.menu",
+  defaultMessage: "header.label.account.menu",
+  description: "header.label.account.menu"
+}), _defineProperty(_defineMessages, "header.links.courses", {
+  id: "header.links.courses",
+  defaultMessage: "Courses",
+  description: "Link to the learner course dashboard"
+}), _defineProperty(_defineMessages, "header.links.programs", {
+  id: "header.links.programs",
+  defaultMessage: "Programs",
+  description: "Link to the learner program dashboard"
+}), _defineProperty(_defineMessages, "header.links.content.search", {
+  id: "header.links.content.search",
+  defaultMessage: "Search",
+  description: "Link to the content search page"
+}), _defineProperty(_defineMessages, "header.links.schools", {
+  id: "header.links.schools",
+  defaultMessage: "Schools & Partners",
+  description: "Link to the schools and partners landing page"
+}), _defineProperty(_defineMessages, "header.user.menu.dashboard", {
+  id: "header.user.menu.dashboard",
+  defaultMessage: "Dashboard",
+  description: "Link to the user dashboard"
+}), _defineProperty(_defineMessages, "header.user.menu.profile", {
+  id: "header.user.menu.profile",
+  defaultMessage: "Profile",
+  description: "Link to the user profile"
+}), _defineProperty(_defineMessages, "header.user.menu.account.settings", {
+  id: "header.user.menu.account.settings",
+  defaultMessage: "Account",
+  description: "Link to account settings"
+}), _defineProperty(_defineMessages, "header.user.menu.order.history", {
+  id: "header.user.menu.order.history",
+  defaultMessage: "Order History",
+  description: "Link to order history"
+}), _defineProperty(_defineMessages, "header.user.menu.logout", {
+  id: "header.user.menu.logout",
+  defaultMessage: "Logout",
+  description: "Logout link"
+}), _defineProperty(_defineMessages, "header.user.menu.login", {
+  id: "header.user.menu.login",
+  defaultMessage: "Login",
+  description: "Login link"
+}), _defineProperty(_defineMessages, "header.user.menu.register", {
+  id: "header.user.menu.register",
+  defaultMessage: "Sign Up",
+  description: "Link to registration"
+}), _defineProperty(_defineMessages, "header.label.account.nav", {
+  id: "header.label.account.nav",
+  defaultMessage: "Account",
+  description: "The aria label for the account menu nav"
+}), _defineProperty(_defineMessages, "header.label.account.menu", {
+  id: "header.label.account.menu",
+  defaultMessage: "Account Menu",
+  description: "The aria label for the account menu trigger"
+}), _defineProperty(_defineMessages, "header.label.account.menu.for", {
+  id: "header.label.account.menu.for",
+  defaultMessage: "Account menu for {username}",
+  description: "The aria label for the account menu trigger when the username is displayed in it"
+}), _defineProperty(_defineMessages, "header.label.main.nav", {
+  id: "header.label.main.nav",
+  defaultMessage: "Main",
+  description: "The aria label for the main menu nav"
+}), _defineProperty(_defineMessages, "header.label.main.menu", {
+  id: "header.label.main.menu",
+  defaultMessage: "Main Menu",
+  description: "The aria label for the main menu trigger"
+}), _defineProperty(_defineMessages, "header.label.main.header", {
+  id: "header.label.main.header",
+  defaultMessage: "Main",
+  description: "The aria label for the main header"
+}), _defineProperty(_defineMessages, "header.label.secondary.nav", {
+  id: "header.label.secondary.nav",
+  defaultMessage: "Secondary",
+  description: "The aria label for the seconary nav"
+}), _defineProperty(_defineMessages, "header.label.skip.nav", {
+  id: "header.label.skip.nav",
+  defaultMessage: "Skip to main content",
+  description: "A link used by screen readers to allow users to skip to the main content of the page."
+}), _defineMessages));
+export default messages;
 //# sourceMappingURL=Header.messages.js.map
